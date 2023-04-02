2fc2365cc831d58f834d57b41514aa8b1346e3ac
cangkevin <cangkevin@gmail.com>
2023-04-02 14:07:35 -0500

    chore: configure prettier

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,6 +1,14 @@
 /** @type {import("prettier").Config} */
 const config = {
-  plugins: [require.resolve("prettier-plugin-tailwindcss")],
-};
+  printWidth: 80,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  semi: false,
+  trailingComma: 'all',
+  importOrder: ['^[~/]'],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+  plugins: [require.resolve('prettier-plugin-tailwindcss')],
+}
 
-module.exports = config;
+module.exports = config
