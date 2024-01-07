c30505b0331399bfaa89e688a41eb4a4a79a81f8
cangkevin <cangkevin@gmail.com>
2024-01-07 00:20:47 -0600

    test: add initial playwright tests

---
+++ tests/index.spec.ts
@@ -0,0 +1,38 @@
+import { expect, test } from '@playwright/test'
+
+import { categories } from '~/pages'
+
+test('home page loads', async ({ page }) => {
+  await page.goto('/')
+
+  await expect(page).toHaveTitle(/SEAshows/)
+
+  await Promise.all(
+    categories.map(async (category) => {
+      await expect(
+        page.getByRole('banner').getByRole('link', { name: category.name }),
+      ).toBeVisible()
+
+      await expect(
+        page.getByRole('main').getByRole('link', { name: category.name }),
+      ).toBeVisible()
+    }),
+  )
+
+  await expect(page.getByRole('contentinfo')).toContainText(
+    /does not store any files/,
+  )
+})
+
+categories.forEach((category) => {
+  test(`navigate to ${category.name} category`, async ({ page }) => {
+    await page.goto('/')
+
+    await page
+      .getByRole('main')
+      .getByRole('link', { name: new RegExp(category.name) })
+      .click()
+
+    await expect(page).toHaveURL(new RegExp(`${category.key}\\?page=1`))
+  })
+})
---
+++ tests/shows.spec.ts
@@ -0,0 +1,16 @@
+import { expect, test } from '@playwright/test'
+
+import { categories } from '~/pages'
+
+categories.forEach((category) => {
+  test(`load ${category.name} shows`, async ({ page }) => {
+    await page.goto(`/shows/${category.key}`)
+
+    await expect(page).toHaveTitle(new RegExp(`${category.key} - Page 1`))
+    await expect(page).toHaveURL(new RegExp(`/shows/${category.key}`))
+
+    await expect(page.getByRole('contentinfo')).toContainText(
+      /does not store any files/,
+    )
+  })
+})
