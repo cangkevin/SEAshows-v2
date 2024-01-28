2008447bb1c08de8a934ed8890026a374c46c97c
cangkevin <cangkevin@gmail.com>
2024-01-27 21:00:15 -0600

    test: add additional asserts

--- processor.ts
+++ processor.ts
@@ -2,16 +2,27 @@ import { type Page } from '@playwright/test'
 
 export async function loadHomePage(page: Page) {
   await page.goto('/')
+
+  await page.getByRole('heading', { name: /SEAShows/ }).isVisible()
 }
 
 export async function loadHKDramaCategoryPage(page: Page) {
   await page.goto('/shows/hk-drama')
+
+  await page.getByRole('heading', { name: /HK Drama/ }).isVisible()
+  await page.locator('img').isVisible()
 }
 
 export async function loadHKVarietyCategoryPage(page: Page) {
   await page.goto('/shows/hk-variety')
+
+  await page.getByRole('heading', { name: /HK Variety/ }).isVisible()
+  await page.locator('img').isVisible()
 }
 
 export async function loadKRDramaCategoryPage(page: Page) {
   await page.goto('/shows/korean-drama-englishsubtitles')
+
+  await page.getByRole('heading', { name: /KR Drama/ }).isVisible()
+  await page.locator('img').isVisible()
 }
--- tests/shows.spec.ts
+++ tests/shows.spec.ts
@@ -8,9 +8,10 @@ categories.forEach((category) => {
 
     await expect(page).toHaveTitle(new RegExp(`${category.name} - Page 1`))
     await expect(page).toHaveURL(new RegExp(`/shows/${category.key}`))
-
     await expect(page.getByRole('contentinfo')).toContainText(
       /does not store any files/,
     )
+
+    await expect(page.locator('img').first()).toBeVisible()
   })
 })
