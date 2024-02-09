047afb01556c501975c12d5f4e1cf6a1a16b210d
cangkevin <cangkevin@gmail.com>
2024-02-08 20:30:32 -0600

    refactor: rename types for feed items

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -2,3 +2,23 @@ export type PaginationLink = {
   linkText: string
   url: string
 }
+
+export type ShowFeedItem = {
+  id: number
+  title: string
+  url: string
+  imageUrl: string
+}
+
+export type EpisodeFeedItem = {
+  id: string | undefined
+  title: string
+  url: string
+  alternateUrls?: EpisodeUrl[]
+  videoSourceUrl?: string
+}
+
+export type EpisodeUrl = {
+  language: string
+  url: string
+}
