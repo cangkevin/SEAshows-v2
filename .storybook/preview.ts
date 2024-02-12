d8e0cf54c9a8872a1ddca99760c1f577577284f7
cangkevin <cangkevin@gmail.com>
2024-02-12 01:49:23 -0600

    chore(Storybook): enable additional viewports for Storybook

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -2,6 +2,7 @@
 // https://storybook.js.org/recipes/tailwindcss#2-provide-tailwind-to-stories
 import '../src/styles/globals.css'
 import { withThemeByClassName } from '@storybook/addon-themes'
+import { INITIAL_VIEWPORTS, MINIMAL_VIEWPORTS } from '@storybook/addon-viewport'
 import { Preview, ReactRenderer } from '@storybook/react'
 
 const preview: Preview = {
@@ -11,6 +12,13 @@ const preview: Preview = {
       matchers: {
         color: /(background|color)$/i,
         date: /Date$/i,
+      },
+    },
+    viewport: {
+      // NOTE - https://storybook.js.org/docs/essentials/viewport#use-a-detailed-set-of-devices
+      viewports: {
+        ...INITIAL_VIEWPORTS,
+        ...MINIMAL_VIEWPORTS,
       },
     },
   },
