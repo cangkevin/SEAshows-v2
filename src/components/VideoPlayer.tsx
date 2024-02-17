34a8233b4ae75b0d12a63a4a51082f485e17bec7
cangkevin <cangkevin@gmail.com>
2024-02-17 02:45:10 -0600

    feat: switch to shadcn/ui AspectRatio component

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -1,3 +1,4 @@
+import { AspectRatio } from './ui/aspect-ratio'
 import { Button } from './ui/button'
 import { useEffect, useState } from 'react'
 import Iframe from 'react-iframe'
@@ -20,13 +21,15 @@ const VideoPlayer = ({ url, sources = [] }: VideoPlayerProps) => {
 
   return (
     <>
-      <Iframe
-        key={videoUrl} // NOTE to prevent from pushing on the history; https://www.aleksandrhovhannisyan.com/blog/react-iframes-back-navigation-bug/
-        url={videoUrl}
-        height='100%'
-        width='100%'
-        className='h-[calc(100vh-8rem)]'
-      />
+      <AspectRatio ratio={16 / 9}>
+        <Iframe
+          key={videoUrl} // NOTE to prevent from pushing on the history; https://www.aleksandrhovhannisyan.com/blog/react-iframes-back-navigation-bug/
+          url={videoUrl}
+          height='99.5%'
+          width='100%'
+        />
+      </AspectRatio>
+
       <div className='flex h-8 flex-row justify-center'>
         {sources.length > 1
           ? sources.map((source) => (
